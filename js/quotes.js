c2ac701393eba0402774fc35b685bb20bf92ff5e
yonnzet <lalala703@naver.com>
2022-03-02 20:19:40 +0900

    todo 기능 구현

---
+++ js/quotes.js
@@ -0,0 +1,38 @@
+const quotes = [
+    {
+        quote:"When you reach the end of your rope, tie a knot in it and hang on.",
+        author:"-Franklin D. Roosevelt",
+    },
+    
+    {
+        quote:"You will face many defeats in life, but never let yourself be defeated. ",
+        author:"-Maya Angelou",
+    },
+    
+    {
+        quote:"Keep smiling, because life is a beautiful thing and there's so much to smile about.",
+        author:"-Marilyn Monroe",
+    },
+    {
+        quote:"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+        author:"-Oprah Winfrey",
+    },
+    {
+        quote:"The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author:"-Nelson Mandela",
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: " -Walt Disney",
+    },
+    
+        
+];
+
+const quote = document.querySelector('.quote div:first-child');
+const author = document.querySelector('.quote div:last-child');
+
+const todayQoute = quotes[Math.floor(Math.random() * quotes.length )]; 
+
+quote.innerText = todayQoute.quote;
+author.innerText = todayQoute.author;
---
+++ js/weekly-todo.js
@@ -0,0 +1,55 @@
+const todoForm = document.getElementById('todo-form');
+const todoInput = todoForm.querySelector('input');
+const todoList = document.getElementById('todo-list');
+
+
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos) );
+}
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintTodo(newTodo){
+    const li = document.createElement('li');
+    li.id = newTodo.id;
+    const span = document.createElement('span');
+    span.innerText = newTodo.text;
+    const button = document.createElement('button');
+    button.innerText = '×';
+    button.addEventListener('click', deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+    todoList.appendChild(li);
+}
+
+function handleTodoSubmit(event) {
+    event.preventDefault();
+    const newTodo = todoInput.value;
+    const newTodObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+    todoInput.value = "";
+    toDos.push(newTodObj);
+    paintTodo(newTodObj);
+    saveToDos();
+}
+
+todoForm.addEventListener("submit", handleTodoSubmit); 
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintTodo);
+}
